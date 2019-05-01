17a7e80419d7d9d344c4945769dc703c5d57c205
Ilya Levin <bskton@gmail.com>
2019-05-02 08:40:49 +1000

    Moving elements between containers.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
@@ -16,5 +17,23 @@ export class AppComponent {
   completed: string[] = [
     'Wash the car',
     'Response to InMail'
-  ]
+  ];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+
+  }
 }
